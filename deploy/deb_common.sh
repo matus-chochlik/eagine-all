e452c2ee11a150629191676cb76386bc20e80fed
Matus Chochlik <chochlik@gmail.com>
2023-07-03 11:31:17 +0200

    Updated how deb packages are build + updated sub-modules

---
+++ deploy/deb_common.sh
@@ -0,0 +1,9 @@
+# Copyright Matus Chochlik.
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#  http://www.boost.org/LICENSE_1_0.txt
+#
+function eagine_deb_installed_size() {
+	du -k -s "${1:-.}" | cut -f 1
+}
+
