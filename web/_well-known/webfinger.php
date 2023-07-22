6c7f9ef29e5a9250ca549a146ba682f7cf610983
Matus Chochlik <chochlik@gmail.com>
2023-07-22 15:48:43 +0200

    Added copyright headers + reformatting in website source files

--- web/_well-known/webfinger.php
+++ web/_well-known/webfinger.php
@@ -1,24 +1,29 @@
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
 <?php
 header('Content-type: application/json');
 if ($_GET["resource"] == "acct:eagine@"+getenv('EAGINE_HOST')) {
 ?>
 {
-	"subject": "acct:eagine@<?php echo getenv('EAGINE_HOST')?>",
+  "subject": "acct:eagine@<?php echo getenv('EAGINE_HOST')?>",
 
-	"aliases": [
+  "aliases": [
     "https://<?php echo getenv('EAGINE_HOST')?>/activitypub/actor"
   ],
 
-	"links": [
-		{
-			"rel": "self",
-			"type": "application/activity+json",
-			"href": "https://<?php echo getenv('EAGINE_HOST')?>/activitypub/actor"
-		}, {
+  "links": [
+    {
+      "rel": "self",
+      "type": "application/activity+json",
+      "href": "https://<?php echo getenv('EAGINE_HOST')?>/activitypub/actor"
+    }, {
       "rel": "http://ostatus.org/schema/1.0/subscribe",
       "template": "https://<?php echo getenv('EAGINE_HOST')?>/activitypub/subscribe?uri={uri}"
     }
-	]
+  ]
 }
 <?php } else { ?>
 {}
--- web/activitypub/subscribe.php
+++ web/activitypub/subscribe.php
@@ -1,3 +1,8 @@
+/// Copyright Matus Chochlik.
+/// Distributed under the Boost Software License, Version 1.0.
+/// See accompanying file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt
+///
 <?php
 header('Content-type: application/json');
 $uri = $_GET["uri"];
