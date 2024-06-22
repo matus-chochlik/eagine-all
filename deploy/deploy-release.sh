f4a4d7be35d50d0153caf93d92efcad76bd10085
Matus Chochlik <chochlik@gmail.com>
2024-06-22 12:20:34 +0200

    Changes in package build scripts + updated sub-modules

--- deploy/deploy-release.sh
+++ deploy/deploy-release.sh
@@ -1,20 +1,18 @@
-#!/bin/bash
+#!/bin/bash -e
 # Copyright Matus Chochlik.
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at
 # https://www.boost.org/LICENSE_1_0.txt
 #
 arch="$(dpkg-architecture -q DEB_HOST_ARCH 2> /dev/null)"
-tempdir="$(mktemp -d)"
 thisdir="$(realpath $(dirname ${0}))"
-function cleanup {
-	rm -rf "${tempdir}"
-}
-trap cleanup EXIT
 #
-cmake --build "$(dirname ${thisdir})" --target eagine-deb-release &&\
-pushd "${tempdir}" &&\
-tar -zxf "${thisdir}/dpkg/release/release.tar.gz" &&\
+pushd "${thisdir}
+cmake --build . --target all
+cpack -G DEB
+
+exit
 find . -type f |\
 	sed "s@^.*\$@put & /sub/eagine/apt/latest/${arch:-amd64}/@" |\
 	sftp oglplus.org@oglplus.org
+popd
--- deploy/dpkg/deb_common.sh
+++
@@ -1,9 +0,0 @@
-# Copyright Matus Chochlik.
-# Distributed under the Boost Software License, Version 1.0.
-# See accompanying file LICENSE_1_0.txt or copy at
-# https://www.boost.org/LICENSE_1_0.txt
-#
-function eagine_deb_installed_size() {
-	du -k -s "${1:-.}" | cut -f 1
-}
-
